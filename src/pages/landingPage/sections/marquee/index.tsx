b395d90450a3b231c03f7a3cfd7840710d7b2b61
hafsat-max <hafsahbusari69@gmail.com>
2023-09-29 17:18:46 +0100

    fixed the slider issue

--- src/pages/landingPage/sections/marquee/index.tsx
+++ src/pages/landingPage/sections/marquee/index.tsx
@@ -1,5 +1,5 @@
-import Ticker from "react-ticker";
 import styles from "./marquee.module.css";
+import Slider from "./slider";
 
 export const Marquee = () => {
   return (
@@ -8,57 +8,10 @@ export const Marquee = () => {
         paddingBlock: "1rem",
         display: "flex",
         flexDirection: "column",
-        height: "520px",
       }}
     >
-      <div style={{ background: "#F9AB00" }} className={styles.marq1}>
-        <Ticker>
-          {({ index }) => (
-            <div className={styles.marquee}>
-              <p>DevFest Ado-Ekiti 2023</p>
-              <svg
-                width="29"
-                height="29"
-                viewBox="0 0 29 29"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="14.2004"
-                  cy="14.2004"
-                  r="14"
-                  transform="rotate(0.826171 14.2004 14.2004)"
-                  fill="black"
-                />
-              </svg>
-            </div>
-          )}
-        </Ticker>
-      </div>
-      <div className={styles.marq2}>
-        <Ticker>
-          {({ index }) => (
-            <div className={styles.marquee}>
-              <p>DevFest Ado-Ekiti 2023</p>
-              <svg
-                width="29"
-                height="29"
-                viewBox="0 0 29 29"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="14.2004"
-                  cy="14.2004"
-                  r="14"
-                  transform="rotate(0.826171 14.2004 14.2004)"
-                  fill="black"
-                />
-              </svg>
-            </div>
-          )}
-        </Ticker>
-      </div>
+      <Slider rotate="-5deg" />
+      <Slider rotate="10deg" />
     </section>
   );
 };
---
+++ src/pages/landingPage/sections/marquee/slider.tsx
@@ -0,0 +1,39 @@
+import styles from "./marquee.module.css";
+
+function Slider({ rotate }: { rotate: "-5deg" | "10deg" }) {
+  return (
+    <div
+      style={{ background: "#F9AB00", transform: `rotate(${rotate})` }}
+      className={`${styles.marq1} ${styles.__ticker}`}
+    >
+      {Array.from({ length: 6 }, () => (
+        <ul style={{ display: "flex", gap: "20px" }}>
+          {Array(10)
+            .fill(0)
+            .map((_, idx) => (
+              <li className={styles.marquee} key={idx}>
+                <p style={{ whiteSpace: "nowrap" }}>DevFest Ado-Ekiti 2023</p>
+                <svg
+                  width="29"
+                  height="29"
+                  viewBox="0 0 29 29"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="14.2004"
+                    cy="14.2004"
+                    r="14"
+                    transform="rotate(0.826171 14.2004 14.2004)"
+                    fill="black"
+                  />
+                </svg>
+              </li>
+            ))}
+        </ul>
+      ))}
+    </div>
+  );
+}
+
+export default Slider;
