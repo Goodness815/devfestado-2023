07809afc52cf1e5826c54a04b49a701d9aba8d46
Favour Dgreat <53022031+Favour-dgreat@users.noreply.github.com>
2023-10-11 11:57:32 +0100

    remove redundant import

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -2,7 +2,6 @@ import LandingLayout from "../../components/layout";
 import { Marquee } from "./sections/marquee";
 import Pitch from "./sections/pitch";
 import Showcase from "./sections/showcase";
-import Speakers from "./sections/speakers";
 import Story from "./sections/story";
 import ThisYear from "./sections/thisYear";
 import Youtube from "./sections/youtube";
