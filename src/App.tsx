ad6d67ea1d281d15b53bf54c893b34e392f02c8c
Elijah Edun <edunelijah18@gmail.com>
2023-10-21 20:26:36 +0100

    fix: dependencies aos and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/landingPage";
-import AOS from 'aos';
+import AOS from "aos";
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
